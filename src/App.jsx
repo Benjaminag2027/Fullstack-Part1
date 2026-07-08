f5fc59ebd7203dac37c41ebbb9f0b211ef012ed0
GigatonProductions <benjaminag.2027@mtchs.org>
2026-07-07 18:33:20 -0600

    lesson7

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,42 @@ import { useState } from 'react'
 let good = 0
 let neutral = 0
 let bad = 0
+let all = 0
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   return (
     <div>
       <h2>give feedback</h2>
-      <button onClick={() => {setGood(good + 1)}}>good</button>
-      <button onClick={() => {setNeutral(neutral + 1)}}>neutral</button>
-      <button onClick={() => {setBad(bad + 1)}}>bad</button>
+      <button onClick={() => {
+          const updatedGood = good + 1
+          setGood(updatedGood)
+          setAll(updatedGood + neutral + bad)
+        }}>good</button>
+      <button onClick={() => {
+          const updatedNeutral = neutral + 1
+          setNeutral(updatedNeutral)
+          setAll(good + updatedNeutral + bad)
+      }}>neutral</button>
+      <button onClick={() => {
+          const updatedBad = bad + 1
+          setBad(updatedBad)
+          setAll(good + neutral + updatedBad)
+      }}>bad</button>
 
       <h2>stastistics</h2>
       <p>
         good {good} <br />
         neutral {neutral} <br />
-        bad {bad}
+        bad {bad} <br />
+        all {all} <br />
+        average {(good - bad) / ((all != 0) ? all : 1)} <br />
+        positive {good * 100 / all} %
         </p>
     </div>
   )
